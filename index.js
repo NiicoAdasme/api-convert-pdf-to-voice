7a2ec9faa2a4b61fc8477d07f7d4b3859a5bc2d2
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-11-10 12:49:59 -0300

    index function y app.yaml

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const dotenv = require('dotenv')
-const path = require('path')
+const index = () => {
 
-const pdfRoutes = require('./src/routes/pdf.routes')
-
-// Initializations
-const app = express()
-dotenv.config()
-
-// Middlewares
-app.use(cors())
-app.use(morgan('dev'))
-app.use(fileUpload({
-    // debug: true,
-    // createParentPath: path.join(__dirname, 'src', 'assets')
-}))
-
-// Directorio publico
-app.use(express.static(path.join(__dirname, 'src', 'assets')));
-
-// Lectura y parseo del body
-app.use(express.json());
-
-// Routes
-app.use('/api', pdfRoutes)
-
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-})
+    const express = require('express')
+    const morgan = require('morgan')
+    const cors = require('cors')
+    const fileUpload = require('express-fileupload')
+    const dotenv = require('dotenv')
+    const path = require('path')
+    
+    const pdfRoutes = require('./src/routes/pdf.routes')
+    
+    // Initializations
+    const app = express()
+    dotenv.config()
+    
+    // Middlewares
+    app.use(cors())
+    app.use(morgan('dev'))
+    app.use(fileUpload({
+        // debug: true,
+        // createParentPath: path.join(__dirname, 'src', 'assets')
+    }))
+    
+    // Directorio publico
+    app.use(express.static(path.join(__dirname, 'src', 'assets')));
+    
+    // Lectura y parseo del body
+    app.use(express.json());
+    
+    // Routes
+    app.use('/api', pdfRoutes)
+    
+    
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+    
+}
