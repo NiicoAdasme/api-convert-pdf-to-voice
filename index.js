03c849eb390d6f98a6c1ba19d7343a4ef9cb431d
NiicoAdasme <nicolas.wladimir@gmail.com>
2022-11-10 11:46:32 -0300

    puerto opcional

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ app.use(express.json());
 app.use('/api', pdfRoutes)
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 })
